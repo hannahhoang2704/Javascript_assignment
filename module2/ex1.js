dfa5a0553c32c58964bc543401b5ab2a36f34979
Hannah Hoang <112083555+hannahhoang2704@users.noreply.github.com>
2022-11-24 18:49:42 +0200

    Update ex1.js

--- module2/ex1.js
+++ module2/ex1.js
@@ -2,7 +2,7 @@
 const number = []
 
 for (let i = 0; i <5; i++){
-  number[i] = prompt("Enter a number:")
+  number[i] = prompt(`Enter 5 numbers. Number ${i+1} :`)
 }
 
 for (let a = (number.length-1); a >=0; a--){
