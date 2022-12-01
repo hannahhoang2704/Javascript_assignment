c4a4edfb6d3f7669d36a4142f6476a99fe9b13f2
Hannah Hoang <112083555+hannahhoang2704@users.noreply.github.com>
2022-12-01 23:27:02 +0200

    Add files via upload

---
+++ module3/t1/1.js
@@ -0,0 +1,7 @@
+const result = document.getElementById('target');
+
+result.innerHTML += '<li>First item</li>';
+result.innerHTML += '<li>Second item</li>';
+result.innerHTML += '<li>Third item</li>';
+
+result.classList.add('my-list')
