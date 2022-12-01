37472cf01130cdec1a365611f892b2e83d8125c6
Hannah Hoang <112083555+hannahhoang2704@users.noreply.github.com>
2022-12-01 23:54:30 +0200

    Add files via upload

---
+++ module3/t11/11.js
@@ -0,0 +1,146 @@
+'use strict';
+const picArray = [
+  {
+    title: 'Title 1',
+    caption: 'Caption 1',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros.',
+    image: {
+      large: 'img/pic1.jpg',
+      medium: 'thumbnails/pic1.jpg',
+    },
+  },
+  {
+    title: 'Title 2',
+    caption: 'Caption 2',
+    description:
+      'Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. ',
+    image: {
+      large: 'img/pic2.jpg',
+      medium: 'thumbnails/pic2.jpg',
+    },
+  },
+  {
+    title: 'Title 3',
+    caption: 'Caption 3',
+    description:
+      'Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. ',
+    image: {
+      large: 'img/pic3.jpg',
+      medium: 'thumbnails/pic3.jpg',
+    },
+  },
+  {
+    title: 'Title 4',
+    caption: 'Caption 4',
+    description:
+      'Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+    image: {
+      large: 'img/pic4.jpg',
+      medium: 'thumbnails/pic4.jpg',
+    },
+  },
+  {
+    title: 'Title 5',
+    caption: 'Caption 5',
+    description:
+      'Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. ',
+    image: {
+      large: 'img/pic5.jpg',
+      medium: 'thumbnails/pic5.jpg',
+    },
+  },
+  {
+    title: 'Title 6',
+    caption: 'Caption 6',
+    description:
+      'Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh.',
+    image: {
+      large: 'img/pic6.jpg',
+      medium: 'thumbnails/pic6.jpg',
+    },
+  },
+  {
+    title: 'Title 7',
+    caption: 'Caption 7',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros.',
+    image: {
+      large: 'img/pic7.jpg',
+      medium: 'thumbnails/pic7.jpg',
+    },
+  },
+  {
+    title: 'Title 8',
+    caption: 'Caption 8',
+    description:
+      'Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. Sed vel velit ante. Aenean quis viverra magna. ',
+    image: {
+      large: 'img/pic8.jpg',
+      medium: 'thumbnails/pic8.jpg',
+    },
+  },
+  {
+    title: 'Title 9',
+    caption: 'Caption 9',
+    description:
+      'Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh. Sed vel velit ante. Aenean quis viverra magna. ',
+    image: {
+      large: 'img/pic9.jpg',
+      medium: 'thumbnails/pic9.jpg',
+    },
+  },
+];
+const selectSection = document.getElementById('pictures');
+
+for (let i= 0; i<picArray.length; i++) {
+  const newArticle = document.createElement('article');       //create element article
+  const newP = document.createElement('p');                     //create element p
+
+  selectSection.appendChild(newArticle);                             //append article to section
+  newArticle.appendChild(newP);
+  newArticle.setAttribute('id', `article-${i+1}`);    //set id for each article
+  newP.appendChild(document.createTextNode(picArray[i].title));      //add heading to element p
+
+
+  const newFig = document.createElement('figure');           //create element figure
+  const newImg = document.createElement('img');              //create element image
+  newImg.src = picArray[i].image.medium;
+  newImg.alt = picArray[i].title;
+  newFig.appendChild(newImg);
+
+  const newCap = document.createElement('figcaption');       //create element figcaption
+  newCap.appendChild(document.createTextNode(picArray[i].caption));
+  newFig.appendChild(newCap);
+
+  newArticle.appendChild(newFig);
+
+  const newDescription = document.createElement('p');
+  newDescription.appendChild(document.createTextNode(picArray[i].description));
+  newArticle.appendChild(newDescription);                              //append description
+
+
+
+}
+
+function modal(evt){
+  evt.preventDefault();
+for (let i= 0; i<picArray.length; i++) {
+  let idOfArticle = `article-${i + 1}`;
+  const callArticle = document.getElementById(`${idOfArticle}`);
+  const dialog = document.querySelector('dialog');            //get the modal
+  const imgOfDialog = document.querySelector('img');
+  const span = document.querySelector('span');
+  callArticle.addEventListener('click', () => {
+    dialog.showModal();
+    imgOfDialog.src = picArray[i].image.large;
+    imgOfDialog.alt = picArray[i].title;
+
+  });
+  span.addEventListener('click', () => {
+    dialog.close();
+  });
+}};
+
+const article = document.querySelector('article');
+article.addEventListener('click', modal);
